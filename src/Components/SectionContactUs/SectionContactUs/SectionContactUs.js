0cd05a2a35ff0da659916c293901bba85f229875
Agnieszka <agnieszka.sekular@gmail.com>
2019-05-30 12:02:27 +0200

    2 commit

--- src/Components/SectionContactUs/SectionContactUs/SectionContactUs.js
+++ src/Components/SectionContactUs/SectionContactUs/SectionContactUs.js
@@ -5,7 +5,10 @@ class SectionContactUs extends Component {
     render() {
       return (
         <div className="container-contactus-section"> 
-            
+          <div className="small-container-contactus">
+            <span>Skontaktuj się z nami</span>
+            <div class="decoration"></div>
+          </div>     
         </div>
       );
     }
